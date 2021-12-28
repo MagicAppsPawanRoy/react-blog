54269471f9b50b4afd23ce3130f514e1ed778b8f
PawanRoy1997 <pawan@magicapps.co.in>
2021-12-28 11:25:05 +0530

    show screen finds the correct blog we are reffering

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
 const ShowScreen = ({ navigation }) => {
+    const { state } = useContext(Context)
+    const blog = state.find((blog) => blog.id === navigation.getParam('id'))
+
     return (
         <View>
-            <Text>Show Screen - {navigation.getParam('id')}</Text>
+            <Text>{blog.title}</Text>
+            <Text>{blog.id}</Text>
         </View>
     )
 }
